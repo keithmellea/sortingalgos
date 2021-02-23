b821fe929ecbe374c133a9b949b0a3194c19c4eb
Keith Mellea <keithmellea@gmail.com>
2021-02-23 15:30:58 -0500

    third commit

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -1,9 +1,11 @@
 // Implement Bubble Sort
 
 function swap(array, idx1, idx2) {
+  debugger
   let temp = array[idx1];  // 2
   array[idx1] = array[idx2];
   array[idx2] = temp;
+  debugger
   // console.log(array);
   return array;
 }
@@ -12,18 +14,26 @@ function bubbleSort(array) {
   let n = array.length - 1;
   let swapped = false;
   for (let i = 1; i <= n; i++) {
+   debugger
     if (array[i - 1] > array[i]) {
+      debugger
       swap(array, i - 1, i);
+      debugger
       // console.log(array);
-
       swapped = true;
     }
-    if (i = n && swapped === false) {
+    debugger
+    if (i === n && swapped === false) {
+      debugger
       return array;
+    
     }
-    else if (i === n && swapped === true) {
+    else if (swapped === true) {
+     debugger
       i = 0;
+      
     }
+    debugger
   }
 }
 
