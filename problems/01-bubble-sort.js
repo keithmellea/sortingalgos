a67c4ed99d93ead26d1fed20335516d937a947fa
jdaniel01 <professor4cob@outlook.com>
2021-02-23 13:33:14 -0800

    done with 1

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -2,44 +2,66 @@
 
 function swap(array, idx1, idx2) {
   debugger
-  let temp = array[idx1];  // 2
+  let temp = array[idx1];
   debugger
   array[idx1] = array[idx2];
   debugger
   array[idx2] = temp;
   debugger
-return array;
 }
 
 function bubbleSort(array) {
-  let n = array.length - 1;
-  let swapped = false;
-  for (let i = 0; i < n; i++) {
+  let swapped = true;
+  debugger
+  while (swapped) {
+    debugger
     swapped = false;
     debugger
-    if (array[i] > array[i + 1]) {
-      debugger
-      swap(array, i, i + 1);
-      debugger
-      // console.log(array);
-      swapped = true;
-    }
-    debugger
-    if (i === n && swapped === false) {
-      debugger
-      return array;
+    for (let i = 0; i < array.length - 1; i++) {
 
+      if (array[i] > array[i + 1]) {
+        debugger
+        swap(array, i, i + 1);
+        swapped = true;
+        debugger
+      }
     }
-    else if (i === n && swapped === true) {
-      debugger
-      i = 0;
-
-    }
-    debugger
   }
+  debugger
+  return array;
 }
-console.log(swap(['a','b','c'], 0, 2))
-console.log('a' < 'b');
+
+// function bubbleSort(array) {
+//   let n = array.length - 1;
+//   let swapped = false;
+//   for (let i = 0; i < n; i++) {
+//     swapped = false;
+//     debugger
+//     if (array[i] > array[i + 1]) {
+//       debugger
+//       swap(array, i, i + 1);
+//       debugger
+//       // console.log(array);
+//       swapped = true;
+//     }
+//     debugger
+//     if (i === n && swapped === false) {
+//       debugger
+//       return array;
+
+//     }
+//     else if (i === n && swapped === true) {
+//       debugger
+//       i = 0;
+
+//     }
+//     debugger
+//   }
+// }
+
+let ar = ['a', 'b', 'c']
+console.log(swap(ar, 0, 2))
+console.log(ar);
 console.log(bubbleSort([1, 2, 5, 6, 2, 3, 8]));
 
 module.exports = {
--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -2,11 +2,26 @@
 
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
-  // your code here
+  let temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
 
 function selectionSort(list) {
-  // your code here
+  let n = list.length;
+
+  for (let i = 1; i < n - 1; i++) {
+    let min = i
+    for (let j = i + 1; j < n; j++) {
+      if (list[j] < list[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      swap(list, list[min], list[i]);
+    }
+  }
+  return list;
 }
 
 module.exports = {
