cbf8a6fddf207aa93246e7c08a74aaae64eb5eb0
Keith Mellea <keithmellea@gmail.com>
2021-02-23 13:39:39 -0500

    second commit

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -4,10 +4,25 @@ function swap(array, idx1, idx2) {
   let temp = array[idx1];
   array[idx1] = array[idx2];
   array[idx2] = temp;
+  return array;
 }
 
 function bubbleSort(array) {
-  // your code here
+  let n = array.length - 1;
+  let swapped = false;
+  for (let i = 0; i < n; i++) {
+    if (array[i] < array[i + 1]) {
+    let swappedFunc = swap(array, array[i], array[i + 1]);
+    let array = swappedFunc;
+    swapped = true;     
+    }
+    if (i = array.length - 1 && swapped === false) {
+      return array;
+    }
+    else if (i === array.length - 1 && swapped === true) {
+      i = 0;
+    }
+  }
 }
 
 module.exports = {
