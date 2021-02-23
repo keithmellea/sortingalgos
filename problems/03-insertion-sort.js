d88db9c7a4786ed434ddef2a7cf8dbd591fdc9b5
jdaniel01 <professor4cob@outlook.com>
2021-02-23 15:48:58 -0800

    near done

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,48 +1,55 @@
 // Implement Insertion Sort
 debugger
 function insertionSort(list) {
-//   debugger
-//   let sorted = 1;
-//   let n = list.length;
-//   let focus = undefined;
-//   debugger
-// let bool = false;
-//   while (sorted < n) {
-//     bool = false;
-//     for (let i = 1; i < n; i++) {
-//       if (focus === undefined) {
-//         focus = i;
-//         debugger
-//       }
+  // debugger
+  // let sorted = 1;
+  // let n = list.length;
+  // let focus = undefined;
+  // debugger
+  // let bool = false;
+  // while (sorted < n) {
+  //   bool = false;
+  //   for (let i = 1; i < n; i++) {
+  //     if (focus === undefined) {
+  //       focus = i;
+  //       debugger
+  //     }
 
-//       if (list[focus] < list[i]) {
-//         focus = i;
-//         debugger
-//       }
-//     }
-//     for (let j = 0; j < sorted; j++) {
+  //     if (list[focus] < list[i]) {
+  //       focus = i;
+  //       debugger
+  //     }
+  //   }
+  //   for (var j = 0; j < sorted; j++) {
 
-//       if (list[focus] > list[j]) {
-//         debugger
-//         list.splice(j + 1, 0, list[focus]);
-//         list.splice(focus, 1);
-//         bool = true;
-//         debugger
-//       }
-//     }
+  //     if (list[focus] > list[j]) {
+  //       debugger
+  //       list.splice(j + 1, 0, list[focus]);
+  //       list.splice(focus, 1);
+  //       bool = true;
+  //       debugger
+  //     }
+  //   }
 
-//    sorted++;
-//   }
-//   return list;
-for (let i = 1; i < list.length; i++) {
-  let currElement = list[i];
-  for (var j = i - 1; j >= 0 && currElement < list[j]; j--) {
-    list[j + 1] = list[j];
+  //   sorted++;
+  // }
+  // return list;
+
+
+
+  for (let i = 1; i < list.length; i++) {
+    let currElement = list[i];
+    for (var j = i - 1; j >= 0 && currElement < list[j]; j--) {
+      list[j + 1] = list[j];
+    }
+    list[j + 1] = currElement;
   }
-  list[j + 1] = currElement;
-}
   return list;
 }
+
+
+
+
 
 // function insertionSort(arr) {
 //   for (let i = 1; i < arr.length; i++) {
--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,11 +1,42 @@
 // Implement Merge Sort
 
 function merge(array1, array2) {
-  // your code here
+  let result = [];
+
+  while (array1.length && array2.length) {
+    if (array1[0] > array2[0]) {
+      result.push(array2[0])
+      array2.splice(0, 1)
+    } else {
+      result.push(array1[0])
+      array1.splice(0, 1);
+    }
+  }
+
+  while (array1.length) {
+    result.push(array1[0]);
+    array1.splice(0, 1);
+  }
+  while (array2.length) {
+    result.push(array2[0])
+    array2.splice(0, 1)
+  }
+  return result;
 }
 
 function mergeSort(array) {
-  // your code here
+  if (array.length === 1) {
+    return array;
+  }
+  let el1 = array.slice(0, Math.floor(array.length / 2));
+  let el2 = array.slice(Math.floor(array.length / 2));
+
+  el1 = mergeSort(el1)
+  el2 = mergeSort(el2)
+
+  return merge(el1, el2);
+
+
 }
 
 module.exports = {
