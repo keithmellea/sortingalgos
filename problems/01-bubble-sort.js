dcaa80d3748c3a09d80a87cd393124feb9808ab8
jdaniel01 <professor4cob@outlook.com>
2021-02-23 10:59:00 -0800

    working on bubble

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -1,29 +1,33 @@
 // Implement Bubble Sort
 
 function swap(array, idx1, idx2) {
-  let temp = array[idx1];
+  let temp = array[idx1];  // 2
   array[idx1] = array[idx2];
   array[idx2] = temp;
+  // console.log(array);
   return array;
 }
 
 function bubbleSort(array) {
   let n = array.length - 1;
   let swapped = false;
-  for (let i = 0; i < n; i++) {
-    if (array[i] < array[i + 1]) {
-    let swappedFunc = swap(array, array[i], array[i + 1]);
-    let array = swappedFunc;
-    swapped = true;     
+  for (let i = 1; i <= n; i++) {
+    if (array[i - 1] > array[i]) {
+      swap(array, i - 1, i);
+      // console.log(array);
+
+      swapped = true;
     }
-    if (i = array.length - 1 && swapped === false) {
+    if (i = n && swapped === false) {
       return array;
     }
-    else if (i === array.length - 1 && swapped === true) {
+    else if (i === n && swapped === true) {
       i = 0;
     }
   }
 }
+
+console.log(bubbleSort([1, 2, 5, 6, 2, 3, 8]));
 
 module.exports = {
   bubbleSort,
