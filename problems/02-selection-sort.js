17a1fae43ba5eae94f1564021adf8291594e2ec4
jdaniel01 <professor4cob@outlook.com>
2021-02-28 11:45:36 -0800

    stuck on insertion sort.

--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -1,28 +1,58 @@
 // Implement Selection Sort
 
 // Implement swap without looking at bubble sort
+
 function swap(arr, index1, index2) {
-  let temp = arr[index1];
-  arr[index1] = arr[index2];
-  arr[index2] = temp;
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
+  return arr;
 }
 
 function selectionSort(list) {
-  let n = list.length;
+  let currIdx = 0;
+  let sorted = 1;
+  let idx = sorted
+  while (sorted < list.length) {
 
-  for (let i = 0; i < n - 1; i++) {
-    let min = i
-    for (let j = i + 1; j < n; j++) {
-      if (list[j] < list[min]) {
-        min = j;
-      }
+    if (list[currIdx] > list[idx]) {
+      currIdx = idx
     }
-    if (min !== i) {
-      swap(list, min, i);
+    if (idx === list.length - 1) {
+      swap(list, sorted - 1, currIdx);
+      sorted++;
+      idx = sorted;
+      currIdx = sorted - 1
+      continue
+
     }
+    if (sorted === list.length) {
+      return list;
+    }
+    idx++;
   }
-  return list;
 }
+
+// function swap(arr, index1, index2) {
+//   let temp = arr[index1];
+//   arr[index1] = arr[index2];
+//   arr[index2] = temp;
+// }
+
+// function selectionSort(list) {
+//   let n = list.length;
+
+//   for (let i = 0; i < n - 1; i++) {
+//     let min = i
+//     for (let j = i + 1; j < n; j++) {
+//       if (list[j] < list[min]) {
+//         min = j;
+//       }
+//     }
+//     if (min !== i) {
+//       swap(list, min, i);
+//     }
+//   }
+//   return list;
+// }
 
 module.exports = {
   selectionSort,
--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,23 +1,63 @@
 // Implement Insertion Sort
 debugger
+// function insertionSort(list) {
+
+
+
+//   for (let i = 1; i < list.length; i++) {
+//     let currElement = list[i];
+//     for (var j = i - 1; j >= 0 && currElement < list[j]; j--) {
+//       list[j + 1] = list[j];
+//     }
+//     list[j + 1] = currElement;
+//   }
+//   return list;
+// }
+
 function insertionSort(list) {
-
-
-
-  for (let i = 1; i < list.length; i++) {
-    let currElement = list[i];
-    for (var j = i - 1; j >= 0 && currElement < list[j]; j--) {
-      list[j + 1] = list[j];
+  let sorted = 0
+  let min = 1
+  while (sorted < list.length) {
+    if (list[min] < list[min - 1]) {
+      let old = [min - 1]
+      [list[min], list[old]] = [list[old], list[min]]
+      min--
+      continue;
     }
-    list[j + 1] = currElement;
+    if (min - 1 <= 0) {
+      min = sorted + 1;
+    } else if (min - 1 === sorted) {
+      if (list[min] > list[min - 1]) {
+        sorted++
+        min = sorted + 1;
+      }
+    }
   }
   return list;
 }
+//   let sortedIdx = 0
+//   let currIdx = 1
 
 
-
-
-
+//   while (sortedIdx < list.length) {
+//     if (list[currIdx] > list[currIdx - 1]) {
+//       sortedIdx++;
+//       currIdx = sortedIdx + 1;
+//     } else {
+//       const newSorted = [list[currIdx - 1]]
+//       if (currIdx - 1 > 0) {
+//         [list[currIdx - 1]] = [list[currIdx]]
+//         [list[currIdx]] = newSorted;
+//         currIdx--;
+//       } else {
+//         [list[currIdx - 1]] = [list[currIdx]]
+//         [list[currIdx]] = newSorted;
+//         sortedIdx++;
+//         currIdx = sortedIdx + 1;
+//       }
+//     }
+//   }
+// }
 // function insertionSort(arr) {
 //   for (let i = 1; i < arr.length; i++) {
 //     let currElement = arr[i];
@@ -28,7 +68,7 @@ function insertionSort(list) {
 //   }
 //   return arr;
 // }
-insertionSort([4, 5, 9, 3, 2, 6, 7])
+console.log(insertionSort([4, 5, 9, 3, 2, 6, 7]))
 
 module.exports = {
   insertionSort
